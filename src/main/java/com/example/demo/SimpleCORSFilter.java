cb4ad801d5b3bbb52f70e9d7a312afa12bb7ea9c
Muhammad Hewedy <mhewedy@gmail.com>
2017-08-05 15:36:38 +0300

    override headers

--- src/main/java/com/example/demo/SimpleCORSFilter.java
+++ src/main/java/com/example/demo/SimpleCORSFilter.java
@@ -16,10 +16,14 @@ public class SimpleCORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+//        response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, soapaction");
-        response.setHeader("Access-Control-Expose-Headers", "Location");
+//        response.setHeader("Access-Control-Expose-Headers", "Location");
+        response.setHeader("Cache-Control", "private");
+        response.setHeader("X-AspNet-Version", "4.0.30319");
+        response.setHeader("X-Powered-By", "ASP.NET");
+        response.setHeader("Server", "Microsoft-IIS/7.5");
         chain.doFilter(req, res);
     }
 
